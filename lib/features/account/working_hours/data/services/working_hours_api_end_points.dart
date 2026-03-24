76a1bfb126f12915f4767ad271c33b756bd1477f
Ahmedkhattab21 <khattabahmed723@gmail.com>
2026-03-24 16:23:12 +0300

    xc

--- lib/features/account/working_hours/data/services/working_hours_api_end_points.dart
+++ lib/features/account/working_hours/data/services/working_hours_api_end_points.dart
@@ -1,5 +1,6 @@
 import 'package:new_waqty_employee_app/core/api/end_points.dart';
 
 class WorkingHoursApiEndPoints {
-  static String getWorkingHours(int page ) => "${EndPoints.baseUrl}api/employee/shifts";
+  static String getWorkingHours(int page) =>
+      "${EndPoints.baseUrl}api/employee/shifts?page=$page";
 }

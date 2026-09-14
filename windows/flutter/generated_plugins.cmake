f2dc85ba2babcaa40cf06af99b215328b7d1e250
Ahmed Khattab <khattabahmed723@gmail.com>
2026-09-14 13:06:36 +0300

    ios

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,6 +5,7 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   connectivity_plus
   file_selector_windows
+  firebase_core
   flutter_secure_storage_windows
   geolocator_windows
   local_auth_windows
@@ -12,6 +13,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  flutter_local_notifications_windows
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)

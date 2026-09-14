d6077098bac037c746051146c3ad0d45692dc7b3
Ahmedkhattab21 <khattabahmed723@gmail.com>
2026-09-14 03:06:48 +0300

    home finished

--- lib/core/api/end_points.dart
+++ lib/core/api/end_points.dart
@@ -1,8 +1,5 @@
 class EndPoints {
-  // Android emulator -> host loopback. iOS/desktop: http://127.0.0.1:8000
-  static const String baseUrl = "http://10.0.2.2:8006";
-  // static const String baseUrl = "https://waqty.alemtayaz.shop/public";
-  // static const String _imageBaseUrl = "storage/app/public/";
+   static const String baseUrl = "http://waqty.rawaealqiyada.com";
 
   static const String employeeAuthRefresh =
       "$baseUrl/api/employee/auth/refresh";
@@ -10,7 +7,4 @@ class EndPoints {
   static const String employeeDeviceToken =
       "$baseUrl/api/employee/device-token";
 
-  // String getImageFromApi(String imageUrl) {
-  //   return baseUrl + _imageBaseUrl + imageUrl;
-  // }
 }
